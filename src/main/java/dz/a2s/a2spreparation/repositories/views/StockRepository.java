7272dd1b59d1f6c58470f16d8eaf9b0eac957114
medkaidi-a2s <m.kaidi@a2s-dz.com>
2025-07-08 18:34:55 +0100

    filtering stocks by companyId

--- src/main/java/dz/a2s/a2spreparation/repositories/views/StockRepository.java
+++ src/main/java/dz/a2s/a2spreparation/repositories/views/StockRepository.java
@@ -33,10 +33,11 @@ public interface StockRepository extends JpaRepository<Stock, StockId> {
                            V.PRD_COLIS AS COLISAGE,
                            ROW_NUMBER() OVER(ORDER BY MED_COMMERCIAL_NAME) AS ROW_NUM
                       FROM PRP_STOCKS V
-                     WHERE LOWER(v.MED_COMMERCIAL_NAME || v.PRD_ATTRIBUT2 || v.PRD_NLOT) LIKE
+                     WHERE V.PRD_CMP_ID = :cmpId
+                       AND LOWER(v.MED_COMMERCIAL_NAME || v.PRD_ATTRIBUT2 || v.PRD_NLOT) LIKE
                            LOWER('%' || :search || '%'))
              WHERE ROW_NUM BETWEEN :start AND :end
             """, nativeQuery = true)
-    List<StockMasterProjection> getAllStocks(@Param("start") Integer start, @Param("end") Integer end, @Param("search") String search);
+    List<StockMasterProjection> getAllStocks(@Param("start") Integer start, @Param("end") Integer end, @Param("cmpId") Integer cmpId, @Param("search") String search);
 
 }
--- src/main/java/dz/a2s/a2spreparation/services/impl/StockServiceImpl.java
+++ src/main/java/dz/a2s/a2spreparation/services/impl/StockServiceImpl.java
@@ -4,6 +4,7 @@ import dz.a2s.a2spreparation.dto.response.PaginatedDataDto;
 import dz.a2s.a2spreparation.dto.stock.response.StockDto;
 import dz.a2s.a2spreparation.mappers.StockMapper;
 import dz.a2s.a2spreparation.repositories.views.StockRepository;
+import dz.a2s.a2spreparation.services.CustomUserDetailsService;
 import dz.a2s.a2spreparation.services.StockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,16 +18,20 @@ import java.util.List;
 public class StockServiceImpl implements StockService {
 
     private final StockRepository stockRepository;
+    private final CustomUserDetailsService customUserDetailsService;
     private final StockMapper stockMapper;
 
     @Override
     public PaginatedDataDto<StockDto> getStocks(Integer page, Integer size, String search) {
         log.info("| Entry | StockService.getStocks | Args | page={}, size={}, search={}", page, size, search);
 
+        Integer cmpId = this.customUserDetailsService.getCurrentCompanyId();
+        log.info("Company Id retrieved {}", cmpId);
+
         int start = (page * size) - size + 1;
         int end = page * size;
 
-        var projections = this.stockRepository.getAllStocks(start, end, search);
+        var projections = this.stockRepository.getAllStocks(start, end, cmpId, search);
         log.info("Fetched the stock master projections from the repo | projections.size={}", projections.size());
 
         var totalRecords = projections.isEmpty() ? 0 : projections.stream().findFirst().get().getTotalCount();
